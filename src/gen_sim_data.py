797bc0af272f02a54cc98976d4264153e140857f
Peter Freese <freese.peter@gmail.com>
2019-01-21 10:30:28 -0800

    sample positive gaussian value

--- src/gen_sim_data.py
+++ src/gen_sim_data.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pprint
 import random
-import copy
+import os
+import math
 
-def hello_world(n_up_down: int,
+def hello_world(n_constant: int,
+                n_up_down: int,
                 median_LFC: float,
                 out_file: str,
                 n_each_class: int = 100):
@@ -15,19 +17,61 @@ def hello_world(n_up_down: int,
     :param out_file:
     :param n_each_class:
     :return:
+
+    s1        u1     u2     u3, ....., u100 d1  d2  d3 ..... d100   u1  u2  ... u10000
     """
     # Go through each of the
     factors_up_down = np.random.normal(loc=median_LFC, scale=median_LFC/10, size=n_up_down)
     # Make sure all are positive
     factors_up_down = replace_any_nonpositive_vals(factors_up_down)
-    pprint.pprint(factors_up_down)
+    # Sort the factors by decreasing order
+    factors_up_down.sort(reverse=True)
+    print(factors_up_down)
 
+    # Open the output file
+    out_dir = os.path.dirname(out_file)
+    print(out_dir)
+    os.makedirs(out_dir, exist_ok = True)
+    out_f = open(out_file, 'w')
 
-def replace_any_nonpositive_vals(vals: [float]):
+    # First, make the header line of column (gene) names
+    up_headers = [f"u{i+1}" for i in range(n_up_down)]
+    down_headers = [f"d{i+1}" for i in range(n_up_down)]
+    no_change_headers = [f"n{i+1}" for i in range(n_constant)]
+    header = "sample\t" + "\t".join(up_headers) + "\t" +\
+             "\t".join(down_headers) + "\t" +\
+             "\t".join(no_change_headers) + "\n"
+    print(header)
+    out_f.write(header)
+
+    # The total number of genes
+    n_genes = n_constant + (2 * n_up_down)
+    # Get uniform log2(TPM) values for each of the genes, between 0 and 10
+    gene_median_tpms_l = [random.random() * 10 for _ in range(n_genes)]
+    # Combine these with the gene header names, indicating whether the gene is
+    # up/down/no-change
+    all_gene_factors = factors_up_down +\
+                       [-1*x for x in factors_up_down] +\
+                       [1 for _ in range(n_constant)] # unchanging genes
+    # each gene_id is like "u1", "50", "l2", "n560", etc.
+    gene_ids = up_headers + down_headers + no_change_headers
+    id_medtpm_factor_t_l = zip(gene_ids, gene_median_tpms_l, all_gene_factors)
+    pprint.pprint(list(id_medtpm_factor_t_l))
+
+    # Go through each sample and write it out
+    for s_i in range(n_each_class):
+        # Go through each of the genes; here, we do not use the factors
+        for _, medtpm, _ in id_medtpm_factor_t_l:
+            tpm = np.random.normal(loc=median_LFC, scale=median_LFC/10, size=n_up_down)
+
+    out_f.close()
+    print(out_file)
+
+def replace_any_nonpositive_vals(vals: [float]) -> [float]:
     """Replace any values that are negative into positive ones"""
     num_not_pos = len([x for x in vals if x <= 0.])
     if num_not_pos == 0:
-        return vals
+        return list(vals)
 
     # Get an array of bools indicating which are not > 0.
     vals = np.array(vals)
@@ -44,16 +88,25 @@ def replace_any_nonpositive_vals(vals: [float]):
     return list(vcp)
 
 
-#hello_world(100, 2, 2., "~/Downloads/outt.pycharm.txt")
+hello_world(1000, 100, 2., "/Users/pfreese/Downloads/outt.pycharm.txt")
+
+def return_pos_gaussian_samp_val(mu: float,
+                                 sigma:float):
+    val = np.random.normal(loc=mu, scale=sigma)
+    if val < 0:
+        # Return a value below 0.01
+        pos_val = math.exp(val/10) / 100
+        return pos_val
+    return val # If val was positive, return its original value
 
 
 
 def test():
     vals = [4, 3, 2, 1, -22, 1, 3, 4, -2]
-    replace_any_nonpositive_vals(vals)
+    #replace_any_nonpositive_vals(vals)
     #neg_vals = vals <= 0.
     #print(neg_vals)
     #print(np.where(neg_vals)[0])
 
-test()
+#test()
 
--- src/test_gen_sim_data.py
+++ src/test_gen_sim_data.py
@@ -1,19 +1,37 @@
 import unittest
 import gen_sim_data
 import random
+import numpy as np
 
 class TestReplacement(unittest.TestCase):
 
     def test_replace_any_nonpositive_vals(self):
         random.seed(2)
-        #vals = [1, 2, 3]
+        ## Test where one negative number is below 0; replace it with ~the
+        ## average of the other
         vals = [1, 2, -5.]
-        exp_cens_vals = [1.0, 2.0, 1.5684]
-        print(exp_cens_vals)
-        censvals = gen_sim_data.replace_any_nonpositive_vals(vals)
-        print(censvals)
-        [self.assertAlmostEqual(censvals[i], exp_cens_vals[i], places=4) for
-         i in range(len(exp_cens_vals))]
+        exp_pos_vals = [1.0, 2.0, 1.5684]
+        vals_w_replace = gen_sim_data.replace_any_nonpositive_vals(vals)
+        # Test that all are approximately as expected
+        [self.assertAlmostEqual(vals_w_replace[i], exp_pos_vals[i], places=4) for
+         i in range(len(vals_w_replace))]
+
+    def test_replace_non_nonpositive_vals(self):
+        ## Test where all are positive and therefore non are replaced
+        vals = [1, 2, 5]
+        self.assertEqual(vals, gen_sim_data.replace_any_nonpositive_vals(vals))
+
+class TestPosGaussianSample(unittest.TestCase):
+
+    def test_pos_gaussian_samp_val(self):
+        np.random.seed(2)
+        # A positive value around 100 sampled as expected
+        val = gen_sim_data.return_pos_gaussian_samp_val(100, 1)
+        self.assertAlmostEqual(val, 99.58324, places=4)
+
+        # A negative value around -100 returned as a positive value close to 0
+        val = gen_sim_data.return_pos_gaussian_samp_val(-100, 1)
+        self.assertAlmostEqual(val, 4.514e-07, places=9)
 
 if __name__ == '__main__':
     unittest.main()
